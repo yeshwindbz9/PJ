8442a986ee06ba0eb6892b0cf136b3c9b5697948
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2022-01-02 20:18:45 +0530

    array difference
    removes all values present in the first list if they are present in the second list, keeping their order.

---
+++ array-diff.js
@@ -0,0 +1,21 @@
+/*
+    Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+    It should remove all values from list a, which are present in list b keeping their order.
+
+    arrayDiff([1,2],[1]) == [2]
+    If a value is present in b, all of its occurrences must be removed from the other:
+    arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
+
+/**
+ * Returns a difference by removing all values from list a, which are present in list b keeping their order.
+ * @param  {Number[]} a An array to subtract from.
+ * @param  {Number[]} b An array to subtract to.
+ * @return {Number[]}      The resultant difference array.
+*/
+function arrayDiff(a, b) {
+  return a.filter(ele=> !b.includes(ele));
+}
+
+let res = arrayDiff([1,2,3], [1,2]);
+console.log(res);

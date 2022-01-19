0453938e30c8c46f821ec5affe2b52b2055c7cb3
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2022-01-20 01:49:29 +0530

    phone num
    Converts an array of ten numbers into a phone number.

---
+++ phone-num.js
@@ -0,0 +1,20 @@
+/*
+    Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+    Example
+    createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+    The returned format must be correct in order to complete this challenge.
+    Don't forget the space after the closing parentheses!
+*/
+
+/**
+ * Converts an array of ten numbers into a phone number.
+ * @param  {Number[]} numbers An input array of 10 integers.
+ * @return {String}   The numbers in the form of a phone number.
+*/
+function createPhoneNumber(numbers){
+  return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')+ '-' + numbers.slice(6).join('')}`;
+}
+
+let res = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+console.log(res);
